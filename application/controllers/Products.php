7c324c624724cd115d2a0deb73ae1e1f5436c295
Kevin <stradtkt22@gmail.com>
2019-02-15 12:51:02 -0500

    Saved product

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -3,21 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Products extends CI_Controller {
 
-    /**
-     * Index Page for this controller.
-     *
-     * Maps to the following URL
-     * 		http://example.com/index.php/welcome
-     *	- or -
-     * 		http://example.com/index.php/welcome/index
-     *	- or -
-     * Since this controller is set as the default controller in
-     * config/routes.php, it's displayed at http://example.com/
-     *
-     * So any other public methods not prefixed with an underscore will
-     * map to /index.php/welcome/<method_name>
-     * @see https://codeigniter.com/user_guide/general/urls.html
-     */
     public function index()
     {
         $this->load->view('products');
@@ -39,7 +24,7 @@ class Products extends CI_Controller {
             redirect('/products/new');
             exit();
         }
-        $this->Product->save_product($this->input->post());
+        $this->product->save_product($this->input->post());
         redirect('/products/finished');
     }
     public function finished_product()
--- application/models/Product.php
+++ application/models/Product.php
@@ -2,8 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Products extends CI_Model {
+
     public function save_product($product_info)
     {
+        $query = "INSERT INTO products (name, price, created_at, updated_at) VALUES (?, ?, NOW(), NOW())";
+        $data = array($product_info['name'], $product_info['price']);
 
+        $this->db->query($query, $data);
     }
 }
